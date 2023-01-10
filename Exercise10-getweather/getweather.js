86ae259b76fa67658b704d8b48d219104d04e6d9
PhuTam54 <phutamytb@gmail.com>
2023-01-10 15:56:43 +0700

    Exercise10

---
+++ Exercise10-getweather/getweather.js
@@ -0,0 +1,9 @@
+$.get("https://api.openweathermap.org/data/2.5/weather?q=Hanoi&appid=dd0c2b39eab1c1d6c2213700353cc121",
+    function (api) {
+        $('.temp').html('Local Temp: ' + api.main.temp + 'C');
+        $('.city').html('City: ' + api.name);
+        $('.description').html('Description: ' + api.weather[0].description);
+
+        $('.icon img').attr('src', 'https://openweathermap.org/img/wn' + api.weather[0].icon + '.png');
+    }
+);

f2a4df7dfe44debeb1803ddc53dae7f0d1da249e
PhuTam54 <phutamytb@gmail.com>
2023-01-13 12:38:35 +0700

    Examination

--- Examination/js/main.js
+++ Examination/js/main.js
@@ -34,3 +34,28 @@ let engine = (id, serial, message) => {
         successIcon[serial].style.opacity = "1";
     }
 };
+
+// Check password
+
+$('#password, #confirmpassword').on('keyup', function(){
+
+    $('.confirm-message').removeClass('success-message').removeClass('error-message');
+
+    let password=$('#password').val();
+    let confirm_password=$('#confirmpassword').val();
+
+    if(password===""){
+        $('.confirm-message').text("Password Field cannot be empty").addClass('error-message');
+    }
+    else if(confirm_password===""){
+        $('.confirm-message').text("Confirm Password Field cannot be empty").addClass('error-message');
+    }
+    else if(confirm_password===password)
+    {
+        $('.confirm-message').text('Password Match!').addClass('success-message');
+    }
+    else{
+        $('.confirm-message').text("Password Doesn't Match!").addClass('error-message');
+    }
+
+});

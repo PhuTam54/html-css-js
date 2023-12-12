c2cfbbc4821122a3e83ec7e2c36234b130d8c35f
PhuTam54 <phutamytb@gmail.com>
2023-12-12 15:39:01 +0700

    Sort middleware via 'helpers'

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const methodOverride = require('method-override');
 const hbs = require('express-handlebars');
-const app = express();
-const port = 3000;
 
-// override with POST having ?_method=DELETE
-app.use(methodOverride('_method'))
+const SortMiddleware = require('./app/middlewares/SortMiddleware');
 
 const route = require('./routes');
-const db = require('./config/db')
+const db = require('./config/db');
 
 // Connect to DB
 db.connect()
 
+const app = express();
+const port = 3000;
+
+// Use static folder
 app.use(express.static('./src/public'));
+
 app.use(express.urlencoded());
 app.use(express.json());
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
+
+// Custom Middlewares
+app.use(SortMiddleware);
 
 // HTTP logger
 // app.use(morgan('combined'))
@@ -28,6 +36,27 @@ app.engine(
         extname: '.hbs',
         helpers: {
             sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+                const sortType = field === sort.column ? sort.type : 'default';
+
+                const icons = {
+                    default: 'fa fa-sort',
+                    asc: 'fas fa-sort-amount-down-alt',
+                    desc: 'fas fa-sort-amount-down',
+                }
+                const types = {
+                    default: 'desc',
+                    asc: 'desc',
+                    desc: 'asc',
+                }
+
+                const icon = icons[sortType]
+                const type = types[sortType]
+
+                return `<a href="/me/stored/course?_sort&column=${field}&type=${type}">
+                    <i class="${icon}"></i>
+                </a>`
+            }
         },
     }),
 );
